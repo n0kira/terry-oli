8130c6f261949fcb306dd58d768e58be535c243f
Nokira <mattiadagosta0@gmail.com>
2025-04-01 22:20:27 +0200

    Non funziona Collezionismo (Template)

--- AlgoBadge/Greedy/collezionismo.cpp
+++ AlgoBadge/Greedy/collezionismo.cpp
@@ -1,26 +1,54 @@
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <numeric>
-#include <set>
-#include <utility>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 void solve(int t) {
     int N, K;
-    cin >> N >> K;
+    // N, Modellini
+    // K, Scaffali
+    cin >> N; 
+    cin >> K;
+
+    cout << "PRESI N(" << N << ") e K(" << K << ")/";
 
     vector<int> C(N);
-    for (int i = 0; i < N; i++) {
+    cout << "CREATO VETTORE N(" << N << ")/";
+    // for (int i = 0; i < N; i++) {
+        
+    // }
+
+    int i=0;
+
+    while(i<N){
+        cout << "PRENDENDO VETTORE : ";
         cin >> C[i];
+        cout << C[i] << " i(" << i << ")/" << endl;
+        i++;
+    }
+    cout << "PRESO VETTORE/";
+
+    // D , Differenza tra valore massimo e minimo dei modellini su uno scaffale
+    // Risposta, somma di tutte le D di ogni scaffale (1x scaffale)
+    sort(C.begin(), C.end());
+    cout << "DOPO SORT/";
+
+    vector<vector<int>> dp(K, vector<int>(N+1));
+    dp[0][0] = 0;
+
+    int D;
+
+    for(int i = 0; i < K; i++){
+        cout << "PRIMO FOR/";
+        for(int j = 0; j < N; j++){
+            cout << "SECODNO FOR/";
+            for(int k = i - 1; k < i; k++){
+                cout << "TERZO FOR/";
+                D = C[j-1] - C[k];
+                dp[i][j] = min(dp[i][j], dp[i-1][k] + D);
+            }
+        }
     }
 
-    // aggiungi codice...
-    int risposta = 42;
+    int risposta = dp[K][N];
 
     cout << "Case #" << t << ": " << risposta << "\n";
 }
@@ -29,8 +57,8 @@ int main() {
     // se preferisci leggere e scrivere da file
     // ti basta decommentare le seguenti due righe:
 
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
     int T;
     cin >> T;
