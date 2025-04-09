874a232e5c7e9e0658c3d3e5e6dbe4cba87648b1
Nokira <mattiadagosta0@gmail.com>
2025-04-09 20:59:49 +0200

    Risolti: Dadi (uguale a FIB_dadi) e Rettangolo

---
+++ Terry-Solves/Rettangolo/rettangolo.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve(int t) {
+    int x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+    cin >> x3 >> y3;
+
+    // aggiungi codice...
+    int x4 = 42, y4 = 42;
+
+    if(x1==x2){
+        x4=x3;
+    }else if(x1==x3){
+        x4=x2;
+    }else{
+        x4=x1;
+    }
+
+    if(y1==y2){
+        y4=y3;
+    }else if(y1==y3){
+        y4=y2;
+    }else{
+        y4=y1;
+    }
+
+
+    cout << "Case #" << t << ": " << x4 << " " << y4 << "\n";
+}
+
+int main() {
+    // se preferisci leggere e scrivere da file
+    // ti basta decommentare le seguenti due righe:
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int T;
+    cin >> T;
+
+    for (int t = 1; t <= T; t++) {
+        solve(t);
+    }
+
+    return 0;
+}
